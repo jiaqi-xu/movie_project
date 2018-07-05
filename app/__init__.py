d5305b24523bcca00a84d93dbb001fe0c9942075
jiaqi-xu <jiaqi.xu.smile@gmail.com>
2018-07-05 10:35:18 -0400

    build backend pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -10,3 +10,8 @@ from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"), 404
